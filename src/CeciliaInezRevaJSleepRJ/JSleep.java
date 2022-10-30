cccf5fcbc40952663a3d0be8fbef43f6a025ac28
ceciliainez11 <cecilia.inez@ui.ac.id>
2022-10-30 21:09:24 +0700

    TP Modul 6

--- src/CeciliaInezRevaJSleepRJ/JSleep.java
+++ src/CeciliaInezRevaJSleepRJ/JSleep.java
@@ -2,6 +2,12 @@ package CeciliaInezRevaJSleepRJ;
 import java.sql.*;
 import java.util.ArrayList;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import com.google.gson.*;
+
 /**
  * Praktikum OOP - CS.
  *
@@ -12,13 +18,34 @@ import java.util.ArrayList;
 
 public class JSleep
 {
-     public static Room createRoom(){
+    public static Room createRoom(){
         Price price = new Price(100000, 0.5);
         Room room = new Room(1, "Hotel", 30, price, Facility.AC, City.DEPOK, "Jalan Margonda Raya");
         return room;
     }
+
+    class Country {
+        public String name;
+        public int population;
+        public List<String> listOfStates;
+    }
     
     public static void main(String[] args){
+
+        String filepath = "C:\\Users\\cecil\\Documents\\kuliah\\semester 3\\oop\\JSleep\\JSleep\\city.json";
+        Gson gson = new Gson();
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(filepath));
+            Country input = gson.fromJson(br, Country.class);
+            System.out.println("name : " + input.name);
+            System.out.println("population: " + input.population);
+            System.out.println("states : ");
+            input.listOfStates.forEach(state -> System.out.println(state));
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
         ArrayList<Room> RoomSerialized = new ArrayList<Room>();
         for(int i = 0; i<5; i++){
             RoomSerialized.add(i, JSleep.createRoom());
